38c9a9024910a18426bb8a7467948a884056835c
Michał Kalbarczyk <fazibear@gmail.com>
2016-08-12 22:47:57 +0200

    Use sigil instead of "

--- test/output_converter_test.exs
+++ test/output_converter_test.exs
@@ -5,7 +5,7 @@ defmodule Test.OutputConverter do
 
   @input "[R] → web/test.ex:1:11 Modules should have a @moduledoc tag."
 
-  @output "{\"type\":\"Issue\",\"location\":{\"path\":\"web/test.ex\",\"lines\":{\"end\":1,\"begin\":1}},\"description\":\"Modules should have a @moduledoc tag.\",\"check_name\":\"Code Readability\",\"categories\":[\"Style\"]}"
+  @output ~s({"type":"Issue","location":{"path":"web/test.ex","lines":{"end":1,"begin":1}},"description":"Modules should have a @moduledoc tag.","check_name":"Code Readability","categories":["Style"]})
 
   test "convert one line input" do
     assert OutputConverter.convert({@input, true}) == @output
@@ -17,7 +17,7 @@ defmodule Test.OutputConverter do
   [C] ↗ web/test.exs:87 There is no whitespace around parentheses/brackets most of the time, but here there is.
   """
 
-  @output "{\"type\":\"Issue\",\"location\":{\"path\":\"web/test.exs\",\"lines\":{\"end\":1,\"begin\":1}},\"description\":\"Modules should have a @moduledoc tag.\",\"check_name\":\"Code Readability\",\"categories\":[\"Style\"]}\0{\"type\":\"Issue\",\"location\":{\"path\":\"web/test.exs\",\"lines\":{\"end\":44,\"begin\":44}},\"description\":\"Pipe chain should start with a raw value.\",\"check_name\":\"Refactoring Oportunities\",\"categories\":[\"Style\"]}\0{\"type\":\"Issue\",\"location\":{\"path\":\"web/test.exs\",\"lines\":{\"end\":87,\"begin\":87}},\"description\":\"There is no whitespace around parentheses/brackets most of the time, but here there is.\",\"check_name\":\"Consistency\",\"categories\":[\"Style\"]}\0"
+  @output ~s({"type":"Issue","location":{"path":"web/test.exs","lines":{"end":1,"begin":1}},"description":"Modules should have a @moduledoc tag.","check_name":"Code Readability","categories":["Style"]}\0{"type":"Issue","location":{"path":"web/test.exs","lines":{"end":44,"begin":44}},"description":"Pipe chain should start with a raw value.","check_name":"Refactoring Oportunities","categories":["Style"]}\0{"type":"Issue","location":{"path":"web/test.exs","lines":{"end":87,"begin":87}},"description":"There is no whitespace around parentheses/brackets most of the time, but here there is.","check_name":"Consistency","categories":["Style"]}\0)
 
   test "convert multi line input" do
     assert OutputConverter.convert({@input, true}) == @output
